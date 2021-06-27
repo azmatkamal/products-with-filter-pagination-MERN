e05bfc7b71677eda13121d8e49d9c4861840c328
Muhammad Azmat Kamal Khan <azmat.kamalkhan@gmail.com>
2021-06-27 23:42:39 +0200

    Initial Setup

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+const helmet = require("helmet");
+var app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+// Body parser middleware
+app.use(bodyParser.urlencoded({ limit: "1000mb", extended: true }));
+app.use(bodyParser.json({ limit: "1000mb", extended: true }));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+    })
+  );
+} else {
+  app.use(logger("dev"));
+}
+
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("X-XSS-Protection", 0);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port: " + port);
+});
